946c8d6d91bc3381bfd223ca189480c20ad236bc
Bohdan Khudiakov <b.khudiakov@gmail.com>
2025-02-09 12:54:22 +0200

    Modified model's description field, added newlines to files

--- cinema/models.py
+++ cinema/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
-    description = models.CharField(max_length=500)
+    description = models.TextField(blank=True, null=True)
     duration = models.PositiveIntegerField()
 
     def __str__(self) -> str:
